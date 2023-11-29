84666b0f03912f0694f8a615c491bf769f6d28be
GuilhermeCatone <guilhermecatone2023@gmail.com>
2023-11-29 13:49:00 -0300

    :tada: init

---
+++ script.js
@@ -0,0 +1,18 @@
+function toggleMode() {
+//ativANDO O BOTÃO
+  const html = document.documentElement
+  html.classList.toggle('ligth')
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+//substituir imagem
+  if(html.classList.contains("ligth")) {
+//img 1 
+    img.setAttribute("src", "./assets/img - light.png")
+  } else {
+    //img 2
+    img.setAttribute("src" , "./assets/img.png")
+  }
+
+  
+}
